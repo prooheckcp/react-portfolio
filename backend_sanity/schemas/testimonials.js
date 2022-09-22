8b726ca4bac6faaa645e754809e0901ae06cd3bc
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-09-22 22:32:21 +0100

    Adding sanity

---
+++ backend_sanity/schemas/testimonials.js
@@ -0,0 +1,27 @@
+export default {
+    name: 'testimonials',
+    title: 'Testimonials',
+    type: 'document',
+    fields:[
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+        {
+            name: 'company',
+            title: 'Company',
+            type: 'string',
+        },
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+    ],
+}
