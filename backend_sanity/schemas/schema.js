af4117828f226dfc0250dd58deb032b624dac2d6
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-09-25 01:43:46 +0100

    Setting up React

--- backend_sanity/schemas/schema.js
+++ backend_sanity/schemas/schema.js
@@ -19,7 +19,14 @@ export default createSchema({
   name: 'default',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([works, testimonials, brands, abouts, skills, workExperience, experiences, contact
-    /* Your types here! */
+  types: schemaTypes.concat([
+    works, 
+    testimonials, 
+    brands, 
+    abouts, 
+    skills, 
+    workExperience, 
+    experiences, 
+    contact
   ]),
 })
