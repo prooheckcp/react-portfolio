e208a496e424bbbbbb7ceb77f5afd3fb43da5df6
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-09-25 03:37:50 +0100

    Finshing mobile support for the navbar

--- front-end-react/src/components/Navbar/Navbar.tsx
+++ front-end-react/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {HiMenuAlt4, HiX} from 'react-icons/hi';
+import {motion} from 'framer-motion'
 
 import './Navbar.scss';
 import {images} from '../../constants'
@@ -6,6 +8,8 @@ import {images} from '../../constants'
 const sections : Array<string> = ['home', 'about', 'work', 'skills', 'contact']
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState<boolean>(false);
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -19,6 +23,28 @@ const Navbar = () => {
         </li>
         )}
       </ul>
+
+      <div className="app__navbar-menu">
+
+        <HiMenuAlt4 onClick={()=>setToggle(true)} />
+        {
+          toggle && (
+            <motion.div
+              whileInView={{x: [300, 0]}}
+              transition={{duration: 0.85, ease:'easeInOut'}}
+            >
+              <HiX onClick={()=> setToggle(false)}/>
+              <ul>
+                {sections.map(item =>
+                    <li key={`item`}>
+                      <a href={`#${item}`} onClick={()=> setToggle(false)}>{item}</a>
+                    </li>
+                  )}
+              </ul> 
+            </motion.div>
+          )
+        }
+      </div>
     </nav>
   )
 }
