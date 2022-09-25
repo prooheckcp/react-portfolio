0b0c577d4450d3550674f1497586d0bf6daafa58
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-09-25 02:21:13 +0100

    Finishing components

--- front-end-react/src/App.tsx
+++ front-end-react/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+// @ts-ignore
 import {About, Footer, Header, Skills, Testimonial, Work} from './container/index.ts';
+// @ts-ignore
+import {Navbar} from './components/index.ts';
 
 const App = () => {
   return (
     <div className='app'>
+      <Navbar />
       <Header />
       <About />
       <Work />
---
+++ front-end-react/src/components/index.ts
@@ -0,0 +1,2 @@
+// @ts-ignore
+export {default as Navbar} from './Navbar/Navbar.tsx';
