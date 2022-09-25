e1b6b2a68de56bff5a048510140b22acd725eb45
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-09-25 02:17:37 +0100

    Setting up all directories

--- front-end-react/src/container/Work/Work.tsx
+++ front-end-react/src/container/Work/Work.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './About.scss';
+import './Work.scss';
 
 const About : React.FC = () => {
   return (
--- front-end-react/src/container/index.ts
+++ front-end-react/src/container/index.ts
@@ -1,6 +1,13 @@
-export {default as About} from './About/About';
-export {default as Footer} from './Footer/Footer';
-export {default as Header} from './Header/Header';
-export {default as Skills} from './Skills/Skills';
-export {default as Testimonial} from './Testimonial/Testimonial';
-export {default as Work} from './Work/Work';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
+export {default as About} from './About/About.tsx';
+// @ts-ignore
+export {default as Footer} from './Footer/Footer.tsx';
+// @ts-ignore
+export {default as Header} from './Header/Header.tsx';
+// @ts-ignore
+export {default as Skills} from './Skills/Skills.tsx';
+// @ts-ignore
+export {default as Testimonial} from './Testimonial/Testimonial.tsx';
+// @ts-ignore
+export {default as Work} from './Work/Work.tsx';
