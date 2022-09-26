26f06ad43bf7b9e17c62d9e0e39edf3ea02272d2
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-09-27 00:35:42 +0100

    Adding experience structure

--- front-end-react/src/container/Skills/Skills.tsx
+++ front-end-react/src/container/Skills/Skills.tsx
@@ -41,6 +41,34 @@ const Skills : React.FC = () => {
             </motion.div>
           ))}
         </motion.div>
+
+        <motion.div
+          className="app__skills-exp"
+        >
+          {experience.works.map(()=>
+            <>
+              <motion.div
+                whileInView={{opacity: [0, 1]}}
+                transition={{duration: 0.5}}
+                className="app__skills-exp-work"
+                data-tip
+                data-for={work.name}
+                key={work.name}
+              >
+                <h4 className="bold-text">{work.name}</h4>
+                <p className="p-text">{work.company}</p>
+              </motion.div>
+              <ReactToolTip
+                id={work.name}
+                effect="solid"
+                arrowColor="#fff"
+                className="skills-tooltip"
+              >
+                {work.desc}
+              </ReactToolTip>
+            </>
+          )}
+        </motion.div>
       </div>
     </>
   )
