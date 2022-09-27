a6ce2b05948de192ca86caa0a2f4520fabecd132
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-09-27 02:52:55 +0100

    Making Typing effect in seperate file

--- front-end-react/src/container/Header/Header.tsx
+++ front-end-react/src/container/Header/Header.tsx
@@ -9,7 +9,8 @@ import FadeIn from '../../components/FadeIn.tsx';
 import {AppWrap} from '../../wrapper/index.ts'
 
 import {images} from '../../constants';
-import Wait from '../../functions/Wait';
+import Wait from '../../functions/Wait.ts';
+import TypingEffect from '../../functions/Typing.ts';
 
 const DELAY_BETWEEN_TITLE : number = 1.2;
 const BAR_DELAY : number = 0.8;
@@ -33,46 +34,8 @@ const scaleVariants = {
 const Header : React.FC = () => {
   const [text, setText] = useState<string>(TITLE_LIST[0])
 
-  const changeText = async (title : string) =>{
-    let currentTitle : string = "";
-
-    for (let i = 0; i < title.length; i++) {
-      currentTitle += title[i];
-      setText(currentTitle+"|");
-      await Wait(TYPING_DELAY)
-    }
-
-    for(let i = 0; i < AMOUNT_OF_BAR_LOOPS; i++){
-      setText(currentTitle+(i%2 === 0 ? "|" : ""));
-      await Wait(BAR_DELAY)
-    }
-  }
-
-  const clearText = async (title : string) =>{
-    let currentTitle : string = title;
-    for (let i = title.length; i >= 0; i--) {
-      currentTitle = currentTitle.slice(0, i);
-      setText(currentTitle+"|");
-      await Wait(TYPING_DELAY)
-    }
-  }
-
-  const textLoop = async ()=>{
-    await clearText(text);
-    await Wait(DELAY_BETWEEN_TITLE/2);
-    for(let i = 0; i < TITLE_LIST.length; i++){
-      let title : string = TITLE_LIST[i];
-
-      await changeText(title);
-      await Wait(DELAY_BETWEEN_TITLE);
-      await clearText(text);
-      await Wait(DELAY_BETWEEN_TITLE/2);      
-    }
-    textLoop();
-  }
-
   useEffect(()=>{
-    setTimeout(textLoop, 2000)
+    TypingEffect(TITLE_LIST, text, setText);
   }, [])
 
   return (
