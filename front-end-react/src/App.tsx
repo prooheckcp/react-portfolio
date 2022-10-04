510ee13482c926b6a7068c9386769e33baee869b
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-10-04 22:45:22 +0100

    Setting up router dependencies

--- front-end-react/src/App.tsx
+++ front-end-react/src/App.tsx
@@ -1,38 +1,35 @@
 import React from 'react';
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 // @ts-ignore
-import {About, Footer, Header, Skills, Testimonial, Work, Background, Wave} from './container/index.ts';
+import {About, Footer, Header, Skills, Testimonial, Work, Wave} from './container/index.ts';
 // @ts-ignore
 import {Navbar} from './components/index.ts';
 import './App.scss'
 
 const App = () => {
   return (
+    <BrowserRouter>
     <div className='app'>
-      {/*
-      <Background />
-  
-    
-      
-    
-      */}
-      
       <Navbar />
-      <Header />
-      <Wave colorSchema="first-fill"/>  
-      <About />
-      <Wave colorSchema="second-fill"/>   
-      <Work />
-      <Wave colorSchema="first-fill"/>  
-      <Skills />   
-      <Wave colorSchema="second-fill"/>  
-      <Testimonial />   
-      <Wave colorSchema="first-fill"/>  
-      <Footer />    
-      
-                 
-
+      <Routes>
+        <Route path="/" element={
+            <>
+                <Header />
+                <Wave colorSchema="first-fill"/>  
+                <About />
+                <Wave colorSchema="second-fill"/>   
+                <Work />
+                <Wave colorSchema="first-fill"/>  
+                <Skills />   
+                <Wave colorSchema="second-fill"/>  
+                <Testimonial />   
+                <Wave colorSchema="first-fill"/>  
+                <Footer />    
+            </>
+          } />
+      </Routes>
     </div>
+    </BrowserRouter>
   )
 }
 
