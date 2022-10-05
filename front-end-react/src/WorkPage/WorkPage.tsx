7a6df5b49a732e1ebe8f2921476954e7c37ef605
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-10-05 02:22:28 +0100

    Loading data into the project page

--- front-end-react/src/WorkPage/WorkPage.tsx
+++ front-end-react/src/WorkPage/WorkPage.tsx
@@ -15,7 +15,7 @@ const WorkPage = () => {
   const [skillsMap, setSkillsMap] = useState<Map<any, any> | null>(null);
   const [usedTech, setUsedTech] = useState<Array<any>>([]);
   const [usedLanguages, setUsedLanguages] = useState<Array<any>>([]);
-  const [worksData, setWorksData] = useState<Map<any, any> | null>(null);
+  const [currentWork, setCurrentWork] = useState(null);
 
   useEffect(()=>{
     FetchSanityData("skills", setSkills);
@@ -34,29 +34,51 @@ const WorkPage = () => {
     }
   }, [skills]);
 
+  const setDataArrays = (currentProject) =>{
+    let techUsed : Array<any> = [];
+    let languagesUsed : Array<any> = [];
+
+    for(let index = 0; index < currentProject.techs.length; index++){
+      const currentTech = currentProject.techs[index];
+      const name : string = currentTech?.trim();
+
+      if(skillsMap?.has(name))
+        techUsed.push(skillsMap?.get(name));
+    }
+
+    for(let index = 0; index < currentProject.languages.length; index++){
+      const currentLanguage = currentProject.languages[index];
+      const name : string = currentLanguage?.trim();
+
+      if(skillsMap?.has(name))
+        languagesUsed.push(skillsMap?.get(name));
+    }
+    setUsedTech(techUsed);
+    setUsedLanguages(languagesUsed);
+  }
+
   useEffect(()=>{
     if(!skillsMap)
       return;
 
-    setUsedLanguages([skillsMap.get('Lua')]);
-    setUsedTech([skillsMap.get('React'), skillsMap.get('React')]);
-  }, [skillsMap])
-
-  useEffect(()=>{
     const query = '*[_type == "works"]';
     client.fetch(query).then(data=>{ 
-      let newMap = new Map();
       for(let index = 0; index < data.length; index++){
-        newMap.set(data[index].id, data[index]);
+        const projectData = data[index]; 
+  
+        if(projectData.id == workIndex){
+          setCurrentWork(projectData);
+          setDataArrays(projectData);
+          break;
+        }
       }
-      setWorksData(newMap);
     });
-  }, [])
+  }, [skillsMap])
 
-  if(!worksData?.has(workIndex))
+  if(!currentWork)
     return '404';
 
-  const currentWork = worksData.get(workIndex);
+  const {description, title, trailerLink} = currentWork;
 
   return (
     <>
@@ -64,10 +86,10 @@ const WorkPage = () => {
         <div className="navbar-gap"/>
         <div className="project-info">
           <h1>
-            Mizukura
+            {title}
           </h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, sed excepturi. Illo ullam excepturi unde recusandae eligendi veniam commodi culpa corrupti, dicta consectetur ea! Ad, non at illo explicabo fugiat adipisci autem voluptates debitis beatae, eveniet incidunt porro earum vero praesentium, animi quidem totam sit! Ducimus asperiores fugit eveniet minima, totam dolor id! Odit quaerat quae saepe dolorum aut natus corporis doloremque, quisquam hic mollitia, at quasi error nostrum accusantium delectus fugiat. Magnam ullam nulla, in inventore debitis perspiciatis! Assumenda, magnam. Porro placeat, nostrum optio sequi molestiae corrupti. Nam quia ipsa fugiat, optio molestiae unde. Eum, sit magni repudiandae architecto autem laboriosam dolorum eaque repellat quaerat dolor! Animi est sed vel necessitatibus placeat similique vitae repellat illo obcaecati recusandae, ex porro reprehenderit, totam adipisci quasi quaerat voluptatibus quidem maxime aut expedita hic nobis. Iusto dolore voluptas quisquam nobis, et quas ullam facilis, eos repudiandae, illo optio? Corporis facere vel numquam, omnis facilis distinctio blanditiis unde quod eligendi, nam rem inventore molestiae aliquam sunt minus architecto nostrum recusandae excepturi? Asperiores debitis aliquid laboriosam unde placeat maiores dolor eveniet provident quod, molestiae repellendus numquam autem. Nesciunt accusamus a ratione officiis excepturi consectetur, officia fugit perferendis, quas saepe et cupiditate ducimus est nisi!
+            {description}
           </p>
         </div>
 
@@ -78,6 +100,22 @@ const WorkPage = () => {
             </div>            
           </a>
         </div>
+
+        {
+          trailerLink ?
+          <>
+            <div className="video-wrapper">
+              <h1>Watch the project's <span>trailer</span> </h1>
+              <ReactPlayer
+                url={`${trailerLink}`}
+                className="react-player"
+                controls
+              />        
+            </div> 
+          </>
+          :
+          ''
+        }
 
         <div className="skills_container">
           <div className="languages info-container">
@@ -90,14 +128,6 @@ const WorkPage = () => {
           </div>
         </div>
 
-        <div className="video-wrapper">
-          <h1>Watch the project's <span>trailer</span> </h1>
-          <ReactPlayer
-            url={`https://www.youtube.com/watch?v=BUd3V0D5d_E}`}
-            className="react-player"
-            controls
-          />        
-        </div>
         <div className="picture-wrapper">
           <h1>Watch the project's <span>pictures</span> </h1>
           <div className="picture-gallery">
