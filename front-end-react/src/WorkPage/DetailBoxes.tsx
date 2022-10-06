7271bdfcb18ba3814d1ae9a8cfb3671d9506c5af
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-10-07 00:03:27 +0100

    Importing all icons

--- front-end-react/src/WorkPage/DetailBoxes.tsx
+++ front-end-react/src/WorkPage/DetailBoxes.tsx
@@ -3,6 +3,15 @@ import {motion} from 'framer-motion';
 import DetailBox from './DetailBox.tsx';
 import {SiRoblox} from 'react-icons/si';
 import {IoCalendarNumber} from 'react-icons/io5';
+import {FiClock} from 'react-icons/fi';
+import {BsFillPersonFill, BsBuilding} from 'react-icons/bs'
+import {IoMdSchool} from 'react-icons/io';
+
+const PROJECT_TYPE = [
+<BsFillPersonFill/>, //Personal
+<IoMdSchool/>, //University
+<BsBuilding/>
+]
 
 const DetailBoxes = () => {
   return (
@@ -10,7 +19,8 @@ const DetailBoxes = () => {
         <div className="detail-boxes">
             <DetailBox text={"hello"} icon={<SiRoblox/>}/>
             <DetailBox text={"hello"} icon={<IoCalendarNumber/>}/>
-            <DetailBox text={"hello"} icon={"a"}/>
+            <DetailBox text={"hello"} icon={<FiClock/>}/>
+            <DetailBox text={"hello"} icon={<IoMdSchool/>}/>
         </div>
     </>
   )
