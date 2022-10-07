54e8fdfead3611082f8d19785e748551c5042149
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-10-07 19:26:55 +0100

    Finishing html part

--- front-end-react/src/container/404/404.tsx
+++ front-end-react/src/container/404/404.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import {Link} from 'react-router-dom';
+import {BiError} from 'react-icons/bi';
 
 const NotFound = () => {
   return (
     <div className="mainContainer">
-        <h1>404 Page not Found</h1>
+        <h1><BiError/></h1>
+        <Link to="/">
+            <h2>404 Page Not Find. Click here to go back</h2>
+        </Link>     
     </div>
   )
 }
