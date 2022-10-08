4c3714cb1b7bf3587d6ba92b5576df0f259d73eb
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2022-10-08 18:48:07 +0100

    Seperating hamburger menu

---
+++ front-end-react/src/components/Navbar/HamburgerMenu.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {HiMenuAlt4, HiX} from 'react-icons/hi';
+import {motion} from 'framer-motion'
+
+const HamburgerMenu = ({setToggle, toggle, sections}) => {
+  return (
+    <>
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={()=>setToggle(true)} />
+        {
+          toggle && (
+            <motion.div
+              whileInView={{x: [300, 0]}}
+              transition={{duration: 0.85, ease:'easeInOut'}}
+            >
+              <HiX onClick={()=> setToggle(false)}/>
+              <ul>
+                {sections.map(item =>
+                    <li key={`item`}>
+                      <a href={`#${item}`} onClick={()=> setToggle(false)}>{item}</a>
+                    </li>
+                  )}
+              </ul> 
+            </motion.div>
+          )
+        }
+      </div>    
+    </>
+  )
+}
+
+export default HamburgerMenu
--- front-end-react/src/components/Navbar/Navbar.tsx
+++ front-end-react/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,9 @@
 import React, {useState} from 'react';
-import {HiMenuAlt4, HiX} from 'react-icons/hi';
-import {motion} from 'framer-motion'
 import './Navbar.scss';
 import {images} from '../../constants'
 import { Link } from 'react-router-dom'; 
 import { HashLink } from 'react-router-hash-link';
+import HamburgerMenu from './HamburgerMenu.tsx';
 
 const sections : Array<string> = ['home', 'about', 'work', 'skills', 'testimonials', 'contact']
 
@@ -31,28 +30,7 @@ const Navbar = () => {
         </li>
         )}
       </ul>
-
-      <div className="app__navbar-menu">
-
-        <HiMenuAlt4 onClick={()=>setToggle(true)} />
-        {
-          toggle && (
-            <motion.div
-              whileInView={{x: [300, 0]}}
-              transition={{duration: 0.85, ease:'easeInOut'}}
-            >
-              <HiX onClick={()=> setToggle(false)}/>
-              <ul>
-                {sections.map(item =>
-                    <li key={`item`}>
-                      <a href={`#${item}`} onClick={()=> setToggle(false)}>{item}</a>
-                    </li>
-                  )}
-              </ul> 
-            </motion.div>
-          )
-        }
-      </div>
+      <HamburgerMenu setToggle={setToggle} toggle={toggle} sections={sections}/>
     </nav>
   )
 }
