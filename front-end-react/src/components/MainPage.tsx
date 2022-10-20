89414ec75dc34160b0f9cdef95c062c3db0c8e0c
Vasco Soares (Prooheckcp) <vasco.soares.2001@gmail.com>
2022-10-20 17:51:46 +0100

    Showing whole main page

--- front-end-react/src/components/MainPage.tsx
+++ front-end-react/src/components/MainPage.tsx
@@ -5,24 +5,17 @@ import {About, Footer, Header, Skills, Testimonial, Work, Wave} from '../contain
 const MainPage = () => {
   return (
     <>
-    {
-      /*
         <Header />
         <Wave id={"about"} colorSchema="first-fill"/>  
         <About />      
-
+        <Wave id={"work"} colorSchema="second-fill"/>   
+        <Work />
         <Wave id={"skills"} colorSchema="first-fill"/>  
         <Skills />   
         <Wave id={"testimonials"} colorSchema="second-fill"/>  
         <Testimonial />   
         <Wave id={"contact"} colorSchema="first-fill"/>  
-        <Footer />          
-      */
-    }
-
-        <Wave id={"work"} colorSchema="second-fill"/>   
-        <Work />
-  
+        <Footer />
     </>
   )
 }
